c61a525a008e0085b5808786a8a790c587983319
Diệp Huy <bahuy140401@gmail.com>
2022-04-17 20:10:31 +0700

    pr

--- backend/src/modules/config/config.service.ts
+++ backend/src/modules/config/config.service.ts
@@ -37,7 +37,7 @@ export class ConfigService {
       VERSION: Joi.string().default('local'),
       NODE_ENV: Joi.string()
         .valid('local', 'development', 'production', 'test', 'provision')
-        .default('production'),
+        .default('local'),
       BACKEND_PORT: Joi.number().default(3030),
       LOGGLY_SUBDOMAIN: Joi.string(),
       LOGGLY_TOKEN: Joi.string(),
